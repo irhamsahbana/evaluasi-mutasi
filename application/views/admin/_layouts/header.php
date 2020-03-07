26ad160ea208a3c24d3c16bb986006de2ab53da2
irhamsahbana <irhamsahbana@gmail.com>
2020-03-08 02:04:51 +0800

    potong template selesai

---
+++ application/views/admin/_layouts/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>Evaluasi Mutasi - PT. PLN (Persero) Unit Induk Wilayah Sulselrabar</title>
+    <!-- Favicon icon -->
+    <link rel="icon" type="image/png" sizes="16x16" href="<?= base_url('assets/admin/'); ?>images/favicon.png">
+    <!-- Custom Stylesheet -->
+    <link href="<?= base_url('assets/admin/'); ?>css/style.css" rel="stylesheet">
+
+</head>
