69378fe268768ca7c39ba87576403b921802ec9f
okti09 <asriokti01@gmail.com>
2020-03-10 10:35:29 +0800

    side bar baru hari ini

--- application/views/admin/_layouts/sidebar.php
+++ application/views/admin/_layouts/sidebar.php
@@ -9,19 +9,30 @@
                         <a href="javascript:void()">
                             <i class="icon-speedometer menu-icon"></i><span class="nav-text">Dashboard</span>
                         </a>
+                    </li>
                     <li>
                         <a href="javascript:void()">
                             <i class="icon-people menu-icon"></i><span class="nav-text">Data Pegawai</span>
                         </a>
-                      
+                    </li>
+                      <li>
+                        <a href="javascript:void()">
+                            <i class="icon-pencil menu-icon"></i> <span class="nav-text">Approval Committee</span>
+                        </a>
+                    </li>
                     <li class="mega-menu mega-menu-sm">
                         <a href="javascript:void()">
                             <i class="icon-list menu-icon"></i><span class="nav-text">Daftar Jabatan</span>
                         </a>
                     </li>
+                      <li>
+                        <a href="javascript:void()">
+                            <i class="icon-note menu-icon"></i><span class="nav-text">Usulan Evaluasi Unit</span>
+                        </a>
+                    </li>
                     <li>
                         <a href="javascript:void()">
-                            <i class="icon-pencil menu-icon"></i> <span class="nav-text">Tanda Tangan</span>
+                            <i class="icon-user-unfollow menu-icon"></i><span class="nav-text">Usulan Evaluasi yang ditolak</span>
                         </a>
                     </li>
                     <li>
