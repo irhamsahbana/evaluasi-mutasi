1deccdfdc175acff2f2293b15fba2c2a5211798e
irhamsahbana <irhamsahbana@gmail.com>
2020-03-29 23:16:17 +0800

    controller baru + folder baru di view

---
+++ application/controllers/Administrator.php
@@ -0,0 +1,22 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class AdministratorInduk extends CI_Controller {
+
+    public function __construct() {
+        parent::__construct();
+        date_default_timezone_set('Asia/Makassar');
+        if($this->session->userdata('status') != "login"){
+            redirect('login');
+        }
+    }
+    public function index()
+    {
+        $data = array(
+            'isi' => 'user/contents/testing',
+            'title' => 'Evaluasi Mutasi - PT. PLN (Persero) Unit Induk Wilayah Sulselrabar', 
+        );
+        $this->load->view('user/_layouts/wrapper', $data);
+    }
+
+}
