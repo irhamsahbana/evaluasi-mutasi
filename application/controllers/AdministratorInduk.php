d73d41eb86e321a24ca35641fe1b13a6e4a1a4ac
okti09 <asriokti01@gmail.com>
2020-03-31 20:33:49 +0800

    ini irham

--- application/controllers/AdministratorInduk.php
+++ application/controllers/AdministratorInduk.php
@@ -177,5 +177,78 @@ class AdministratorInduk extends CI_Controller {
         $this->session->set_flashdata('info', 'Data Personnel Subarea Berhasil Diupdate');
         redirect('AdministratorInduk/tampilanDaftarPersonnelSubarea');
     }
+    
+    public function tampilanApprovalCommittee()
+    {
+        $data = array(
+            'isi' => 'user/contents/administrator_induk/AdministratorInduk',
+            'title' => 'Evaluasi Mutasi - PT. PLN (Persero) Unit Induk Wilayah Sulselrabar', 
+            'data_pegawai' => $this->Crud->ga('tb_approval_committee'),
+        );
+        $this->load->view('user/_layouts/wrapper', $data);
+    }
 
+    public function doAddPegawai() {
+        $input        = $this->input->post(NULL, TRUE);
+        $filenya      = $_FILES['file_ttd']['name'];
+
+        if($filenya = ''){
+        $this->session->set_flashdata('info', 'File Tidak Terpilih'); 
+
+        if($kode == '1'){
+                redirect('AdministratorInduk/approval_committee');
+            }else{
+                redirect('admin');
+            }
+
+        }else{
+            if($kode == '1'){
+                $config['upload_path'] = './asset/user/approval_committee';
+
+         }
+            $config['allowed_types'] = 'jpg|png|jpeg';
+            $config['max_size'] = '0';
+
+            $this->load->library('upload', $config);
+
+            if(!$this->upload->do_upload('file_ttd')){
+                // die();
+                $this->session->set_flashdata('info', 'Upload File Gagal, Periksa Ukuran dan Ekstensi');
+                redirect('AdministratorInduk/tampilanApprovalCommittee/'.$kode);
+            }else{
+                $filenya =  $this->upload->data('file_name');
+            }
+
+        $data_pegawai = array(
+            'nip'                       => $input['nipeg'],
+            'nama_approval'             => $input['nama_approval'],
+            'file_ttd'                  => $input['file_ttd'],
+        );
+        $this->db->set('id_approval', 'UUID()', FALSE);
+        $this->db->insert('tb_approval_committee', $data_pegawai);
+        redirect('AdministratorInduk/tampilanApprovalCommittee');
+      }
+    }
+
+    public function doDeletePegawai($id){
+        $where = array('id_approval' => $id,);
+
+        $this->Crud->d('tb_approval_committee', $where);
+        $this->session->set_flashdata('info', 'Data Pegawai Telah Dihapus');
+        redirect('AdministratorInduk/tampilanApprovalCommittee');
+    }
+
+    public function doUpdatePegawai($id){
+        $where         = array('id_approval' => $id);
+        $input         = $this->input->post(NULL, TRUE);
+        $data_pegawai  = array(
+            'nip'                       => $input['nipeg'],
+            'nama_approval'             => $input['nama_approval'],
+            'file_ttd'                  => $input['file_ttd'],
+            
+        );
+        $this->Crud->u('tb_approval_committee', $data_pegawai, $where);
+        $this->session->set_flashdata('info', 'Data Pegawai Berhasil Diupdate');
+        redirect('AdministratorInduk/tampilanApprovalCommittee');
+    }
 }
