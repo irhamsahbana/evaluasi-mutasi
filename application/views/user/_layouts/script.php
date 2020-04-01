ef59d2482a650e0e5be514f1a946d5077bf7fb70
Ghina Rania <ghinarania14@gmail.com>
2020-04-01 17:31:36 +0800

    pindah script

--- application/views/user/_layouts/script.php
+++ application/views/user/_layouts/script.php
@@ -6,12 +6,12 @@
     <script src="<?= base_url('assets/admin/'); ?>js/settings.js"></script>
     <script src="<?= base_url('assets/admin/'); ?>js/gleek.js"></script>
     <script src="<?= base_url('assets/admin/'); ?>js/styleSwitcher.js"></script>
+    <script src="<?= base_url('assets/admin/'); ?>js/jquery-3.4.1.js"></script>
     <!-- script table -->
     <script src="<?= base_url('assets/admin/'); ?>plugins/tables/js/jquery.dataTables.min.js"></script>
     <script src="<?= base_url('assets/admin/'); ?>plugins/tables/js/datatable/dataTables.bootstrap4.min.js"></script>
     <script src="<?= base_url('assets/admin/'); ?>plugins/tables/js/datatable-init/datatable-basic.min.js"></script>
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script type="text/javascript">
         $(document).ready(function(){
             /* Populate data to subarea dropdown */
