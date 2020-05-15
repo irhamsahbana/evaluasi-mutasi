65827caef35b1486c5838d6615b54400c54c6828
Ghina Rania <ghinarania14@gmail.com>
2020-05-16 06:27:04 +0800

    menu usulan evaluasi untuk admin unit

--- application/controllers/AdministratorUnit.php
+++ application/controllers/AdministratorUnit.php
@@ -12,7 +12,88 @@ class AdministratorUnit extends CI_Controller {
     }
     public function index()
     {
-        $data;
+        $data = array(
+            'isi' => 'user/contents/testing',
+            'title' => 'Evaluasi Mutasi - PT. PLN (Persero) Unit Induk Wilayah Sulselrabar', 
+        );
+        $this->load->view('user/_layouts/wrapper', $data);
     }
+
+# ************ Begin Menu Lembar Evaluasi ******************
+    public function tampilanUsulanLembarEvaluasi(){
+        $where = array('status_usulan' => 'diterima');
+
+        $data = array(
+            'isi'         => 'user/contents/administrator_unit/tabelUsulanLembarEvaluasi',
+            'title'       => 'Evaluasi Mutasi - PT. PLN (Persero) Unit Induk Wilayah Sulselrabar',
+            'lembar_evaluasi_diterima' => $this->Crud->gw('tb_usulan_evaluasi', $where), 
+        );
+
+        $this->load->view('user/_layouts/wrapper', $data);
+    }
+
+    public function tampilanAddUsulanLembarEvaluasi(){
+        $data = array(
+            'isi'         => 'user/contents/administrator_unit/addUsulanLembarEvaluasi',
+            'title'       => 'Evaluasi Mutasi - PT. PLN (Persero) Unit Induk Wilayah Sulselrabar', 
+            'area'        => $this->Crud->ga('tb_business_area'),
+            'approval'    => $this->M_AdministratorUnit->getDataApproval(),
+            'posisi'      => $this->Crud->ga('tb_posisi_approval_committee'),
+        );
+
+        $this->load->view('user/_layouts/wrapper', $data);
+    }
+
+    public function autoFillUsulanPegawai(){
+        $id_pegawai = $this->input->post('nip',FALSE);
+        $dataPeg = $this->M_AdministratorUnit->getPegawaiById($id_pegawai)->result();
+        echo json_encode($dataPeg);
+    }
+
+    public function doAddUsulan() {
+        $id_administrator = $this->session->userdata('id_administrator');
+        $tgl_usulan       = date('y-m-d h:i:s');
+        $id_usulan        = str_replace(' ', '_', $tgl_usulan);
+        $no_surat         = '-';
+        $nip              = $this->input->post('nip_usulan');
+        $jabatan          = $this->input->post('jabatan');
+        $id_approval      = $this->input->post('nama_usulan_approval');
+        $posisi           = $this->input->post('posisi');
+
+        $data_usulan = array(
+            'id_usulan'            => $id_usulan,
+            'id_administrator'     => $id_administrator,
+            'tgl_usulan'           => $tgl_usulan,
+            'no_surat'             => $no_surat,
+            'status_usulan'        => 'diterima',
+            'alasan_ditolak'       => '-',
+        );
+        $this->db->insert('tb_usulan_evaluasi', $data_usulan);
+
+        for($count=0; $count<count($nip); $count++){
+            $data_pegawai = array(
+                'id_usulan'                      => $id_usulan,
+                'nip'                            => $nip[$count],
+                'id_sebutan_jabatan_usulan'      => $jabatan[$count],
+                'keterangan'                     => '-',
+            );
+            $this->db->insert('tb_usulan_evaluasi_pegawai', $data_pegawai);
+        }
+
+        for($count=0; $count<count($id_approval); $count++){
+            $data_approval = array(
+                'id_usulan'         => $id_usulan,
+                'id_approval'       => $id_approval[$count],
+                'id_posisi'         => $posisi[$count],
+            );
+            $this->db->insert('tb_usulan_evaluasi_approval', $data_approval);
+        }
+
+        $this->session->set_flashdata('alert_success', 'Usulan Telah Ditambahkan!');
+        redirect('AdministratorUnit/tampilanUsulanLembarEvaluasi');
+    }
+
+# ************ End Menu Lembar Evaluasi ******************
+
 
 }
--- application/models/M_AdministratorUnit.php
+++ application/models/M_AdministratorUnit.php
@@ -2,4 +2,21 @@
 class M_AdministratorUnit extends CI_Model {
     
     public function ca($t){return $this->db->get($t)->num_rows();} //count all
+
+    public function getDataApproval() {
+        return $this->db->from('tb_approval_committee')
+          ->join('tb_pegawai', 'tb_pegawai.nip=tb_approval_committee.nip', 'left')
+          ->get()
+          ->result();
+    }
+
+    public function getPegawaiById($id) {
+        $this->db->from('tb_pegawai');
+        $this->db->join('tb_jabatan', 'tb_jabatan.id_sebutan_jabatan=tb_pegawai.id_sebutan_jabatan', 'left');
+        $this->db->join('tb_personnel_area', 'tb_personnel_area.personnel_subarea=tb_jabatan.personnel_subarea', 'left');
+        $this->db->join('tb_business_area', 'tb_business_area.business_area=tb_personnel_area.business_area', 'left');
+        $this->db->where(array('nip' =>  $id));
+        $query = $this->db->get();
+        return $query;
+    }
 }
