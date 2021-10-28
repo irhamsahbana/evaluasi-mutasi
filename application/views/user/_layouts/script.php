ab864796eeea2bf11bf97b8492e62296fa75fe69
irhamsahbana <irhamsahbana@gmail.com>
2021-10-28 12:15:49 +0800

    fixing bug: nilai_talenta undefined for dynamic element, change it's index from 0 to 2

--- application/views/user/_layouts/script.php
+++ application/views/user/_layouts/script.php
@@ -951,9 +951,9 @@
                                 async : true,
                                 dataType : 'json',
                                 success : function(talenta){
-                                    $('#n_smstr_1_'+count_pegawai).val(talenta[2]['nilai_talenta']).attr('title', talenta[2]['tahun_talenta']+' Semester '+talenta[2]['semester_talenta']);
-                                    $('#n_smstr_2_'+count_pegawai).val(talenta[1]['nilai_talenta']).attr('title', talenta[1]['tahun_talenta']+' Semester '+talenta[1]['semester_talenta']);
                                     $('#n_smstr_3_'+count_pegawai).val(talenta[0]['nilai_talenta']).attr('title', talenta[0]['tahun_talenta']+' Semester '+talenta[0]['semester_talenta']);
+                                    $('#n_smstr_2_'+count_pegawai).val(talenta[1]['nilai_talenta']).attr('title', talenta[1]['tahun_talenta']+' Semester '+talenta[1]['semester_talenta']);
+                                    $('#n_smstr_1_'+count_pegawai).val(talenta[2]['nilai_talenta']).attr('title', talenta[2]['tahun_talenta']+' Semester '+talenta[2]['semester_talenta']);
                                 }
                             });
                         });   
